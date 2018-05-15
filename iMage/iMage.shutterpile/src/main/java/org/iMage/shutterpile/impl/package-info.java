8115787683d52b85220c4d83f6197980b70c0a50
LarsGavris <lars.gavris@student.kit.edu>
2018-05-15 20:06:30 +0200

    deleted all old files

--- iMage/iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains the implementation for the interfaces in {@link org.iMage.shutterpile.port}
- * 
- * Dominic Wolff
- *
- */
-package org.iMage.shutterpile.impl;
--- iMage/iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/supplier/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains all {@link org.iMage.shutterpile.port.IWatermarkSupplier
- * IWatermarkSuppliers} for this module.
- *
- * @author Dominic Wolff
- *
- */
-package org.iMage.shutterpile.impl.supplier;
